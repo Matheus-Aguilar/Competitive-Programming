bfa380f75c4c3fa064b83ed864486f089f2d23f2
Matheus Aguilar <matheusdeoliveira.ti@gmail.com>
2020-05-10 22:01:23 -0300

    Added UFV-Treinamento

---
+++ UFV-Treinamento/Contest 2020-05-07/b.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define optimize ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define ll long long
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+#define pii pair<int, int>
+#define PI 3.141592653589793238462643383279502884L
+#define mod % 1000000007
+#define all(v) v.begin(), v.end()
+#define ms(x, y) memset(x, y, sizeof(x))
+
+using namespace std;
+
+int main(){
+    optimize;
+    
+    int x;
+    cin >> x;
+
+    if(x % 5 == 0)
+        cout << x/5 << "\n";
+    else
+        cout << x/5 + 1 << "\n";
+
+    return 0;   
+}
---
+++ UFV-Treinamento/Contest 2020-05-07/c.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define optimize ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define ll long long
+#define INF 0x3f3f3f3f
+#define INFLL 0x3f3f3f3f3f3f3f3f
+#define pii pair<int, int>
+#define PI 3.141592653589793238462643383279502884L
+#define mod % 1000000007
+#define all(v) v.begin(), v.end()
+#define ms(x, y) memset(x, y, sizeof(x))
+
+using namespace std;
+
+int main(){
+    optimize;
+
+    int n, ans = 0, sum = 0;
+    cin >> n;
+    while(n--){
+        int a, b;
+        cin >> a >> b;
+        sum += b;
+        sum -= a;
+        ans = max(ans, sum);
+    }
+
+    cout << ans << "\n";
+    
+    return 0;   
+}
